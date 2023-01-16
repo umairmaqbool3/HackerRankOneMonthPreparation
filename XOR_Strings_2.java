e370564b0db60da4f633fb943f5e05a83c366f3d
Umair Maqbool <umair.maqbool55@gmail.com>
2023-01-16 21:11:25 +0500

    XOR Strings 2

---
+++ XOR_Strings_2.java
@@ -0,0 +1,11 @@
+ public static String stringsXOR(String s, String t) {
+        String res = new String("");
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == t.charAt(i))
+                res += "0";
+            else
+                res += "1";
+        }
+
+        return res;
+    }
